211c1134c1517e28b8de5e994c6c361d08299ac7
DESKTOP-SQ34IUF\achre <a.haddaji@bio-aware.com>
2020-12-16 12:10:03 +0100

    New test fixtures.

--- BioloMICS.ClientApi.Tests/SearchTests.cs
+++ BioloMICS.ClientApi.Tests/SearchTests.cs
@@ -5,36 +5,48 @@ using BioloMICS.ClientApi.Model;
 using BioloMICS.ClientApi.Tests.Model;
 using NUnit.Framework;
 using System.Linq;
-using System.Collections.Generic;
-using System.Text;
 
 namespace BioloMICS.ClientApi.Tests
 {
 	[TestFixture]
-	class SearchTests
+	class ClientTests
 	{
 		[Test]
-		public void SearchForStrainsTest() 
+		public void SchemasTest() 
 		{
-			//https://webservices.bio-aware.com/mirri
-			//Queryable<TestModel>.Not(x => x.TestProperty, QueryOperationEnum.NbrIsEqualTo, 0)
-			//			.And(x => x.TestProperty, QueryOperationEnum.NbrIsSmallerThan, 0)
-			//			.Or(x => x.TestProperty, QueryOperationEnum.NbrIsDifferentFrom, 0);
-
-
 			var client = new BiolomicsClient(baseUri: "http://localhost:52145", new ClientCredentials { ClientId = "xx", ClientSecret = "xx" });
 			
 			var schemas = client.GetSchemas();
 
 			Assert.IsTrue(schemas.Any(x=> x.WebsiteId == 87) && schemas.First(x=>x.WebsiteId == 87).TableViews.Any(x=> x.TableViewName == "Strains"));
+		}
+
+		[Test]
+		public void GetRecordByIdTest()
+		{
+			var client = new BiolomicsClient(baseUri: "http://localhost:52145", new ClientCredentials { ClientId = "xx", ClientSecret = "xx" });
 
 			var repository = client.GetRepository(websiteId: 87);
 
 			var result = repository.GetRecordById<StrainsModel>(id: 3);
 
-			Assert.IsTrue(result.Id == 3);
-
 			var result2 = repository.GetRecordById(tableView: "Strains", id: 3);
+
+			Assert.IsTrue(result.Id == 3);
+		}
+
+		[Test]
+		public void SearchTest()
+		{
+			//Example.
+			//Queryable<TestModel>.Not(x => x.TestProperty, QueryOperationEnum.NbrIsEqualTo, 0)
+			//			.And(x => x.TestProperty, QueryOperationEnum.NbrIsSmallerThan, 0)
+			//			.Or(x => x.TestProperty, QueryOperationEnum.NbrIsDifferentFrom, 0);
+
+			var client = new BiolomicsClient(baseUri: "http://localhost:52145", new ClientCredentials { ClientId = "xx", ClientSecret = "xx" });
+
+			var repository = client.GetRepository(websiteId: 87);
+			
 
 			var result3 = repository.Search(Queryable<StrainsModel>.Not(x => x.Id, QueryOperationEnum.NbrIsEqualTo, 3));
 
